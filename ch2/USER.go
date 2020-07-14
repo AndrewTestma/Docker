741c2370d757d4b8ec33c6facd02f8f62833ee0a
AndrewTestma <425780724@qq.com>
2020-07-14 11:25:41 +0800

    user add uid/gid

--- ch2/USER.go
+++ ch2/USER.go
@@ -18,6 +18,20 @@ func main() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Geteuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getegid(),
+				Size:        1,
+			},
+		},
 	}
 
 	if err := cmd.Run(); err != nil {
